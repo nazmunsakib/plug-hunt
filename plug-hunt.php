946ac70069d0143b80010fc327816b953a6d3e78
Nazmun Sakib <wp.coder3@gmail.com>
2022-04-24 17:26:40 +0600

    commit

--- plug-hunt.php
+++ plug-hunt.php
@@ -9,54 +9,56 @@
  * Text Domain: phunt
  */
 
-if  ( !defined( 'ABSPATH' ) )exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-if ( is_admin() ){
-    require_once dirname( __FILE__ ) . '/inc/admin/profile.php';
+if ( is_admin() ) {
+	require_once dirname( __FILE__ ) . '/inc/admin/profile.php';
 }
 
 /**
- * author Bio 
+ * author Bio
  * Parametar @$contact
-*/
-function phunt_author_bio( $contact ){
-    global $post;
+ */
+function phunt_author_bio( $contact ) {
+	global $post;
 
-    $author = get_user_by('id', $post->post_author );
+	$author = get_user_by( 'id', $post->post_author );
 
-    $bio = get_user_meta( $author->ID, 'description', true );
-    $facebook = get_user_meta( $author->ID, 'facebook', true );
-    $twitter = get_user_meta( $author->ID, 'twitter', true );
-    $linkedin = get_user_meta( $author->ID, 'linkedin', true );
-    
-    ob_start();
-    ?>
+	$bio      = get_user_meta( $author->ID, 'description', true );
+	$facebook = get_user_meta( $author->ID, 'facebook', true );
+	$twitter  = get_user_meta( $author->ID, 'twitter', true );
+	$linkedin = get_user_meta( $author->ID, 'linkedin', true );
+
+	ob_start();
+	?>
 
     <div class="author-wrapper">
         <div class="author-image">
-            <?php echo get_avatar( $author->ID, 96 );  ?>
+			<?php echo get_avatar( $author->ID, 96 ); ?>
         </div>
         <div class="author-info">
             <h3><?php echo $author->display_name; ?></h3>
             <p><?php echo esc_html( $bio ); ?></p>
             <ul class="author-social">
-                <li><a href="<?php echo esc_url( $facebook ); ?>"><?php _e('Facebook', 'phunt' ); ?></a></li>
-                <li><a href="<?php echo esc_url( $twitter ); ?>"><?php _e('Twitter', 'phunt' ); ?></a></li>
-                <li><a href="<?php echo esc_url( $linkedin ); ?>"><?php _e('Linkedin', 'phunt' ); ?></a></li>
+                <li><a href="<?php echo esc_url( $facebook ); ?>"><?php _e( 'Facebook', 'phunt' ); ?></a></li>
+                <li><a href="<?php echo esc_url( $twitter ); ?>"><?php _e( 'Twitter', 'phunt' ); ?></a></li>
+                <li><a href="<?php echo esc_url( $linkedin ); ?>"><?php _e( 'Linkedin', 'phunt' ); ?></a></li>
             </ul>
         </div>
     </div>
 
-    <?php
-    $markup = ob_get_clean();
+	<?php
+	$markup = ob_get_clean();
 
-    return $contact.$markup;
+	return $contact . $markup;
 }
 
 add_filter( 'the_content', 'phunt_author_bio' );
 
-function phunt_plugin_script(){
-    wp_enqueue_style('phunt-style', plugins_url('assets/css/style.css', __FILE__ ));
+function phunt_plugin_script() {
+	wp_enqueue_style( 'phunt-style', plugins_url( 'assets/css/style.css', __FILE__ ) );
 }
 
-add_action('wp_enqueue_scripts', 'phunt_plugin_script');
+add_action( 'wp_enqueue_scripts', 'phunt_plugin_script' );
